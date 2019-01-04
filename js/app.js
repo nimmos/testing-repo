379ae329cfb62d924c9374f2a14ee1bac5145265
Daniel Sánchez de Vera <dsanchezdevera@lemurinthebox.com>
2019-01-04 12:01:24 +0100

    Adding styles.

--- js/app.js
+++ js/app.js
@@ -7,4 +7,16 @@ element.appendChild(sub);
 $(document).ready( function() {
 
 	$('#test button').html('Subscribe to Git');
+
+	$('#test button').css('color', 'blue');
+	$('#test button').css('background-color', 'transparent');
+	$('#test button').css('border', 'solid 1px grey');
+	$('#test button').css('border-radius', '5px');
+	$('#test button').css('cursor', 'pointer');
+	$('#test button').hover( function () {
+		$(this).css('background-color', 'silver');
+	}, function () {
+		$(this).css('background-color', 'transparent');
+	});
+
 });
